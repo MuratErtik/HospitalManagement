3d2084c053022f19fb58c06926c272cd125644c4
Murat Ertik <dmuratertik1@gmail.com>
2025-07-26 08:14:10 -0700

    Jwt Validator and Provider classes have been created.

---
+++ src/main/java/com/hospitalmanagementsystem/demo/config/JWT_CONSTANT.java
@@ -0,0 +1,8 @@
+package com.hospitalmanagementsystem.demo.config;
+
+public class JWT_CONSTANT {
+
+    public static final String SECRET_KEY = "dfdssdsklvdsndnvdfddssdddsdsdsdssaslnvkfnkdnkfdkfdfkdnfdjknfknjgnfkfgnjdksweoimewiodklvnkldnvkdnvldknvkldnvldnkvldnvldnvdklnv";
+
+    public static final String JWT_HEADER = "Authorization";
+}
