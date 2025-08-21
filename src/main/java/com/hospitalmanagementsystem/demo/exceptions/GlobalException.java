45c3ca6326b4b519ab41747d41479cf4b9b211db
Murat Ertik <dmuratertik1@gmail.com>
2025-08-21 10:29:01 -0700

    patient exception has been completed.

--- src/main/java/com/hospitalmanagementsystem/demo/exceptions/GlobalException.java
+++ src/main/java/com/hospitalmanagementsystem/demo/exceptions/GlobalException.java
@@ -42,5 +42,20 @@ public class GlobalException {
 
     }
 
+    @ExceptionHandler(PatientException.class)
+    public ResponseEntity<ErrorDetail> handlePatientException(PatientException de, WebRequest request) {
+
+        ErrorDetail errorDetail = new ErrorDetail();
+
+        errorDetail.setTimestamp(LocalDateTime.now());
+
+        errorDetail.setError(de.getMessage());
+
+        errorDetail.setDetails(request.getDescription(false));
+
+        return new ResponseEntity<>(errorDetail, HttpStatus.BAD_REQUEST);
+
+    }
+
 
 }
