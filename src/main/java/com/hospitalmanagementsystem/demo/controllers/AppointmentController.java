004a89cc9ce6605cf9c99626fadb42b69c150085
Murat Ertik <dmuratertik1@gmail.com>
2025-08-24 09:48:24 -0700

    doctor able to add new appointment slots according to schedule

--- src/main/java/com/hospitalmanagementsystem/demo/controllers/AppointmentController.java
+++ src/main/java/com/hospitalmanagementsystem/demo/controllers/AppointmentController.java
@@ -5,8 +5,10 @@ import com.hospitalmanagementsystem.demo.config.JwtProvider;
 import com.hospitalmanagementsystem.demo.exceptions.DoctorException;
 import com.hospitalmanagementsystem.demo.requests.CompleteDoctorInformationsRequest;
 import com.hospitalmanagementsystem.demo.requests.CompleteDoctorScheduleRequest;
+import com.hospitalmanagementsystem.demo.requests.CreateSlotRequest;
 import com.hospitalmanagementsystem.demo.responses.CompleteDoctorInfoResponse;
 import com.hospitalmanagementsystem.demo.responses.CompleteDoctorScheduleResponse;
+import com.hospitalmanagementsystem.demo.responses.CreateAppointmentSlotResponse;
 import com.hospitalmanagementsystem.demo.services.AppointmentService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -31,7 +33,22 @@ public class AppointmentController {
         Long doctorId = jwtProvider.getUserIdFromToken(jwt);
 
         if(role.equals("DOCTOR") && doctorId.equals(userId) ){
-            CompleteDoctorScheduleResponse response = appointmentService.completeDoctorScheduleResponse(request, userId);
+            CompleteDoctorScheduleResponse response = appointmentService.generateDoctorScheduleResponse(request, userId);
+            return ResponseEntity.status(HttpStatus.OK).body(response);
+        }else{
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+        }
+    }
+
+    @PostMapping("/{userId}/add-appointment/slot")
+    public ResponseEntity<CreateAppointmentSlotResponse> complete(@RequestHeader("Authorization") String jwt,
+                                                                  @PathVariable Long userId,
+                                                                  @RequestBody CreateSlotRequest request) throws DoctorException {
+        String role = jwtProvider.getUserRoleFromToken(jwt);
+        Long doctorId = jwtProvider.getUserIdFromToken(jwt);
+
+        if(role.equals("DOCTOR") && doctorId.equals(userId) ){
+            CreateAppointmentSlotResponse response = appointmentService.createAppointmentSlot(request, userId);
             return ResponseEntity.status(HttpStatus.OK).body(response);
         }else{
             return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
--- src/main/java/com/hospitalmanagementsystem/demo/services/AppointmentService.java
+++ src/main/java/com/hospitalmanagementsystem/demo/services/AppointmentService.java
@@ -1,16 +1,25 @@
 package com.hospitalmanagementsystem.demo.services;
 
+import com.hospitalmanagementsystem.demo.entities.AppointmentSlot;
 import com.hospitalmanagementsystem.demo.entities.Doctor;
 import com.hospitalmanagementsystem.demo.entities.DoctorSchedule;
 import com.hospitalmanagementsystem.demo.entities.User;
 import com.hospitalmanagementsystem.demo.exceptions.DoctorException;
+import com.hospitalmanagementsystem.demo.repositories.AppointmentSlotRepository;
 import com.hospitalmanagementsystem.demo.repositories.DoctorRepository;
 import com.hospitalmanagementsystem.demo.repositories.DoctorScheduleRepository;
 import com.hospitalmanagementsystem.demo.repositories.UserRepository;
 import com.hospitalmanagementsystem.demo.requests.CompleteDoctorScheduleRequest;
+import com.hospitalmanagementsystem.demo.requests.CreateSlotRequest;
 import com.hospitalmanagementsystem.demo.responses.CompleteDoctorScheduleResponse;
+import com.hospitalmanagementsystem.demo.responses.CreateAppointmentSlotResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 
 @Service
 @RequiredArgsConstructor
@@ -22,8 +31,10 @@ public class AppointmentService {
 
     private final UserRepository userRepository;
 
+    private final AppointmentSlotRepository appointmentSlotRepository;
+
     //complete doctor schedule
-    public CompleteDoctorScheduleResponse completeDoctorScheduleResponse(CompleteDoctorScheduleRequest req, Long doctorId) throws DoctorException {
+    public CompleteDoctorScheduleResponse generateDoctorScheduleResponse(CompleteDoctorScheduleRequest req, Long doctorId) throws DoctorException {
 
         User user = userRepository.findByUserId(doctorId);
 
@@ -57,6 +68,93 @@ public class AppointmentService {
 
         return response;
 
+    }
+
+    public CreateAppointmentSlotResponse createAppointmentSlot(CreateSlotRequest req, Long doctorId) throws DoctorException {
+
+
+        User user = userRepository.findByUserId(doctorId);
+
+        if(user == null) throw new DoctorException("user not found with id "+doctorId);
+
+        Doctor doctor = doctorRepository.findDoctorByUser(user);
+
+        if(doctor == null) throw new DoctorException("Doctor not found with id "+doctorId);
+
+        DoctorSchedule doctorSchedule = doctorScheduleRepository.findDoctorScheduleByDoctor(doctor);
+
+        if(doctorSchedule == null) throw new DoctorException("Doctor Schedule has not found with id "+doctorId);
+
+        DayOfWeek doctorOffDayOfWeek = doctorSchedule.getDayOfWeek();
+
+
+        for (LocalDate date = req.getStartDate();!date.isAfter(req.getEndDate());date = date.plusDays(1)) {
+
+            DayOfWeek day = date.getDayOfWeek();
+
+            if(day== DayOfWeek.SATURDAY || day==DayOfWeek.SUNDAY || day==doctorOffDayOfWeek){
+                continue;
+            }
+
+            generateSlotForDay(doctorSchedule,date);
+
+        }
+
+        CreateAppointmentSlotResponse response = new CreateAppointmentSlotResponse();
+
+        response.setMessage("Successfully created appointment slots");
+
+        return response;
+
+    }
+
+    private void generateSlotForDay(DoctorSchedule doctorSchedule, LocalDate date) {
+        final int duration = doctorSchedule.getSlotDurationMinutes();
+
+        LocalTime time = doctorSchedule.getStartTime();
+        final LocalTime dayEnd = doctorSchedule.getEndTime();
+
+        final LocalTime lunchStart = LocalTime.NOON;        // 12:00
+        final LocalTime lunchEnd   = LocalTime.of(13, 0);   // 13:00
+
+        while (!time.plusMinutes(duration).isAfter(dayEnd)) {
+
+            //if it is in lunch break update time as 13.00 and continue
+            boolean overlapsLunch = time.isBefore(lunchEnd) && time.plusMinutes(duration).isAfter(lunchStart);
+            if (overlapsLunch) {
+                time = lunchEnd;
+                continue;
+            }
+
+            LocalDateTime newStartTime = LocalDateTime.of(date, time);
+            LocalDateTime newEndTime   = newStartTime.plusMinutes(duration);
+
+            // Overlap Control: existing.start < newEnd && existing.end > newStart
+            boolean overlap = appointmentSlotRepository
+                    .existsByDoctorScheduleAndStartTimeLessThanAndEndTimeGreaterThan(
+                            doctorSchedule,
+                            newEndTime,
+                            newStartTime
+                    );
+
+            if (!overlap) {
+                // Make sure that no duplication
+                boolean sameStartExists =
+                        appointmentSlotRepository.existsByDoctorScheduleAndStartTime(doctorSchedule, newStartTime);
+
+                if (!sameStartExists) {
+                    AppointmentSlot slot = new AppointmentSlot();
+                    slot.setDoctorSchedule(doctorSchedule);
+                    slot.setStartTime(newStartTime);
+                    slot.setEndTime(newEndTime);
+                    slot.setBooked(false);
+
+                    appointmentSlotRepository.save(slot);
+                }
+            }
+
+            time = time.plusMinutes(duration);
+        }
     }
 
 }
