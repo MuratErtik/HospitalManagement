95ccc1f225688c0e5af09a0d9f95a464a88fc9e7
Murat Ertik <dmuratertik1@gmail.com>
2025-08-24 09:46:53 -0700

    findDoctor method has been added.

--- src/main/java/com/hospitalmanagementsystem/demo/repositories/DoctorScheduleRepository.java
+++ src/main/java/com/hospitalmanagementsystem/demo/repositories/DoctorScheduleRepository.java
@@ -1,7 +1,10 @@
 package com.hospitalmanagementsystem.demo.repositories;
 
+import com.hospitalmanagementsystem.demo.entities.Doctor;
 import com.hospitalmanagementsystem.demo.entities.DoctorSchedule;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface DoctorScheduleRepository extends JpaRepository<DoctorSchedule, Long> {
+    DoctorSchedule findDoctorScheduleByDoctor(Doctor doctor);
+
 }
