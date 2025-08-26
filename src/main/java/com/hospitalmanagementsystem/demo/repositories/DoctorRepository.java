6561aa6f9bffb29246e8936e32fd743f7f2f86e5
Murat Ertik <dmuratertik1@gmail.com>
2025-08-26 05:27:02 -0700

    find by department has been added.

--- src/main/java/com/hospitalmanagementsystem/demo/repositories/DoctorRepository.java
+++ src/main/java/com/hospitalmanagementsystem/demo/repositories/DoctorRepository.java
@@ -1,12 +1,17 @@
 package com.hospitalmanagementsystem.demo.repositories;
 
+import com.hospitalmanagementsystem.demo.entities.Department;
 import com.hospitalmanagementsystem.demo.entities.Doctor;
 import com.hospitalmanagementsystem.demo.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 
 public interface DoctorRepository extends JpaRepository<Doctor, Integer> {
 
     Doctor findDoctorByUser(User user);
 
     Doctor findDoctorByDoctorId(Long id);
+
+    List<Doctor> findDoctorByDepartment(Department department);
 }
