ac0d949a1c90b4ebb13be30ea788b024301ae56a
Murat Ertik <dmuratertik1@gmail.com>
2025-08-26 10:32:46 -0700

    default repo has been created.

---
+++ src/main/java/com/hospitalmanagementsystem/demo/repositories/AppointmentStatusRepository.java
@@ -0,0 +1,9 @@
+package com.hospitalmanagementsystem.demo.repositories;
+
+import com.hospitalmanagementsystem.demo.entities.AppointmentStatus;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AppointmentStatusRepository extends JpaRepository<AppointmentStatus, Long> {
+
+    AppointmentStatus findByAppointmentStatus(String appointmentStatus);
+}
