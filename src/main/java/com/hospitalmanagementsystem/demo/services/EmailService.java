248e57711bb4fcfb1ed12ce44b18ce8c37f770de
Murat Ertik <dmuratertik1@gmail.com>
2025-08-26 10:29:16 -0700

    appointment confirm mail able to send.

--- src/main/java/com/hospitalmanagementsystem/demo/services/EmailService.java
+++ src/main/java/com/hospitalmanagementsystem/demo/services/EmailService.java
@@ -1,5 +1,6 @@
 package com.hospitalmanagementsystem.demo.services;
 
+import com.hospitalmanagementsystem.demo.entities.Appointment;
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
@@ -32,5 +33,29 @@ public class EmailService {
             throw new MailSendException("failed to send mail");
         }
 
+    }
+
+    public void makeAppointment(String userEmail, String name, String lastname, Appointment appointment) throws MessagingException {
+
+        try {
+            MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+            MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage,"utf-8");
+            String subject = "Your Appointment has been confirmed";
+            String text = "Dear " + name + " " + lastname   + " Welcome to the KOU Hospital. We are so glad to see you here. \nYour appointment has been completed successfully. \n" +
+                    "-----------Details----------\n" +
+                    "Start Time:" + appointment.getSlot().getStartTime() + "\n" +
+                    "End Time:" + appointment.getSlot().getEndTime() + "\n" +
+                    "Doctor Name:" + appointment.getSlot().getDoctorSchedule().getDoctor().getUser().getName() + " " + appointment.getSlot().getDoctorSchedule().getDoctor().getUser().getLastname() + "\n" +
+                    "Department:" + appointment.getSlot().getDoctorSchedule().getDoctor().getDepartment().getDepartmentName() + "\n" +
+                    " Have a great day!";
+
+            messageHelper.setSubject(subject);
+            messageHelper.setText(text);
+            messageHelper.setTo(userEmail);
+
+            javaMailSender.send(mimeMessage);
+        } catch (MailException e) {
+            throw new MailSendException("failed to send mail");
+        }
     }
 }
