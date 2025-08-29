b825fb1248e407a3b756ac28cb83ea0775bedd73
Murat Ertik <dmuratertik1@gmail.com>
2025-08-28 17:49:39 -0700

    Prescription able to adding into db.

--- src/main/java/com/hospitalmanagementsystem/demo/controllers/MedicineController.java
+++ src/main/java/com/hospitalmanagementsystem/demo/controllers/MedicineController.java
@@ -3,12 +3,10 @@ package com.hospitalmanagementsystem.demo.controllers;
 import com.hospitalmanagementsystem.demo.config.JwtProvider;
 import com.hospitalmanagementsystem.demo.exceptions.AuthException;
 import com.hospitalmanagementsystem.demo.exceptions.MedicineException;
+import com.hospitalmanagementsystem.demo.requests.CreatePrescriptionRequest;
 import com.hospitalmanagementsystem.demo.requests.InsertMedicineRequest;
 import com.hospitalmanagementsystem.demo.requests.SignupRequest;
-import com.hospitalmanagementsystem.demo.responses.CompleteDoctorScheduleResponse;
-import com.hospitalmanagementsystem.demo.responses.ListMedicineResponse;
-import com.hospitalmanagementsystem.demo.responses.MedicineResponse;
-import com.hospitalmanagementsystem.demo.responses.SignupResponse;
+import com.hospitalmanagementsystem.demo.responses.*;
 import com.hospitalmanagementsystem.demo.services.MedicineService;
 import jakarta.mail.MessagingException;
 import lombok.RequiredArgsConstructor;
@@ -43,7 +41,7 @@ public class MedicineController {
     }
 
     @GetMapping("/listAll")
-    public ResponseEntity<List<ListMedicineResponse>> insertMedicine( @RequestHeader("Authorization") String jwt,@RequestParam(required = false) String medicineName) throws MedicineException {
+    public ResponseEntity<List<ListMedicineResponse>> insertMedicine(@RequestHeader("Authorization") String jwt,@RequestParam(required = false) String medicineName) throws MedicineException {
 
         String role = jwtProvider.getUserRoleFromToken(jwt);
 
@@ -55,5 +53,16 @@ public class MedicineController {
         }
 
 
+    }
+    @PostMapping("/addPrescription/{appId}")
+    public ResponseEntity<PrescriptionResponse> addPrescription(@RequestHeader("Authorization") String jwt,@RequestBody CreatePrescriptionRequest req,@PathVariable Long appId) throws MedicineException {
+
+        String role = jwtProvider.getUserRoleFromToken(jwt);
+        if(role.equals("DOCTOR")){
+            PrescriptionResponse response = medicineService.createPrescription(appId,req);
+            return ResponseEntity.status(HttpStatus.OK).body(response);
+        }else{
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+        }
     }
 }
--- src/main/java/com/hospitalmanagementsystem/demo/services/MedicineService.java
+++ src/main/java/com/hospitalmanagementsystem/demo/services/MedicineService.java
@@ -1,15 +1,27 @@
 package com.hospitalmanagementsystem.demo.services;
 
 
+import com.hospitalmanagementsystem.demo.entities.Appointment;
+import com.hospitalmanagementsystem.demo.entities.AppointmentStatus;
 import com.hospitalmanagementsystem.demo.entities.Medicine;
+import com.hospitalmanagementsystem.demo.entities.Prescription;
+import com.hospitalmanagementsystem.demo.exceptions.AppointmentException;
 import com.hospitalmanagementsystem.demo.exceptions.MedicineException;
+import com.hospitalmanagementsystem.demo.exceptions.PrescriptionException;
+import com.hospitalmanagementsystem.demo.repositories.AppointmentRepository;
+import com.hospitalmanagementsystem.demo.repositories.AppointmentStatusRepository;
 import com.hospitalmanagementsystem.demo.repositories.MedicineRepository;
+import com.hospitalmanagementsystem.demo.repositories.PrescriptionRepository;
+import com.hospitalmanagementsystem.demo.requests.CreatePrescriptionRequest;
 import com.hospitalmanagementsystem.demo.requests.InsertMedicineRequest;
 import com.hospitalmanagementsystem.demo.responses.ListMedicineResponse;
 import com.hospitalmanagementsystem.demo.responses.MedicineResponse;
+import com.hospitalmanagementsystem.demo.responses.PrescriptionResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -18,6 +30,12 @@ import java.util.stream.Collectors;
 public class MedicineService {
 
     private final MedicineRepository medicineRepository;
+
+    private final PrescriptionRepository prescriptionRepository;
+
+    private final AppointmentRepository appointmentRepository;
+
+    private final AppointmentStatusRepository appointmentStatusRepository;
 
     // insert medicine
     public MedicineResponse insertMedicine(InsertMedicineRequest request) {
@@ -73,6 +91,63 @@ public class MedicineService {
         medicineResponse.setDuration(medicine.getDuration());
 
         return medicineResponse;
+    }
+
+    //create prescription and complete appointment
+    public PrescriptionResponse createPrescription(Long appointmentId, CreatePrescriptionRequest request) {
+
+        Prescription prescription = new Prescription();
+
+        List<Medicine> prescriptionMedicines = new ArrayList<>();
+
+        for (InsertMedicineRequest req: request.getMedicines()) {
+
+            Medicine medicine = medicineRepository.findByMedicineNameAndDosage(req.getMedicineName(), req.getDosage());
+
+            if (medicine == null) {
+                throw new MedicineException("Medicine does not exist with medicine name: " + req.getMedicineName()+"and dosage: " + req.getDosage());
+
+            }
+
+            prescriptionMedicines.add(medicine);
+        }
+
+
+        prescription.setMedicines(prescriptionMedicines);
+
+        prescription.setPrescriptionDate(LocalDateTime.now());
+
+        prescription.setNotes(request.getNote());
+
+        Appointment appointment = appointmentRepository.findByAppointmentId(appointmentId);
+
+        if (appointment == null) {
+            throw new AppointmentException("Appointment does not exist with appointmentId: " + appointmentId);
+        }
+
+        if (appointment.getStatus().getAppointmentStatus().equals("COMPLETED")) {
+
+            throw new PrescriptionException("Appointment already completed with appointmentId: " + appointmentId);
+        }
+
+        prescription.setAppointment(appointment);
+
+        AppointmentStatus appointmentStatus = appointmentStatusRepository.findByAppointmentStatus("COMPLETED");
+
+        appointment.setStatus(appointmentStatus);
+
+        appointment.setPrescription(prescription);
+
+        prescriptionRepository.saveAndFlush(prescription); // dive into later...
+
+        appointmentRepository.save(appointment);
+
+        PrescriptionResponse prescriptionResponse = new PrescriptionResponse();
+
+        prescriptionResponse.setMessage("Prescription created successfully");
+
+        return prescriptionResponse;
+
     }
 
 
